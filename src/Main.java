8cc691a9edb1e532a7fc3bd197e034488f178cce
Mathew <mmartens28@yahoo.com>
2025-05-18 13:24:35 -0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        var sherlockHolmes = new Person(
+                "Sherlock",
+                "Holmes",
+                "221B Baker Street",
+                "London",
+                "NW16XE");
+
+        sherlockHolmes.PrintDetails();
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,63 @@
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String streetAddress;
+    private String city;
+    private String zipcode;
+
+    public Person(String firstName, String lastName, String streetAddress, String city, String zipcode) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.zipcode = zipcode;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public void PrintDetails(){
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Street Address: " + streetAddress);
+        System.out.println("City: " + city);
+        System.out.println("Zipcode: " + zipcode);
+    }
+}
