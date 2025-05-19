20b889da5f872bb5b7dfa8fe2a212739b17070f1
Mathew <mmartens28@yahoo.com>
2025-05-18 20:29:59 -0500

    remove unused getters and setters

--- src/Person.java
+++ src/Person.java
@@ -13,46 +13,6 @@ public class Person {
         this.zipcode = zipcode;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getStreetAddress() {
-        return streetAddress;
-    }
-
-    public void setStreetAddress(String streetAddress) {
-        this.streetAddress = streetAddress;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
-
     public void PrintDetails(){
         System.out.println("First Name: " + firstName);
         System.out.println("Last Name: " + lastName);
